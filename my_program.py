c51a94902b55137c1b931ddf2cf9a9ca2a6cc2fd
shylpa.sharma <shylpa.sharma@gmail.com>
2020-11-02 18:05:54 +0000

    Pushing python program to check system details

---
+++ my_program.py
@@ -0,0 +1,7 @@
+import socket
+
+print ("Program to check the hostname and IP of the system")
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname)    
+print("Hostname is:" + hostname)    
+print("IP Address is:" + IPAddr)  
